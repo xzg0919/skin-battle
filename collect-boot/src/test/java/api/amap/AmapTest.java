c4fa298196e4bb12f53ceb5f47d76f9d649822a4
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-06-05 17:27:43 +0800

    高德测试代码

--- collect-boot/src/test/java/api/amap/AmapTest.java
+++ collect-boot/src/test/java/api/amap/AmapTest.java
@@ -3,6 +3,7 @@ package api.amap;
 import io.itit.itf.okhttp.FastHttpClient;
 import io.itit.itf.okhttp.Response;
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class AmapTest extends TestCase {
 
@@ -12,11 +13,26 @@ public class AmapTest extends TestCase {
      * 测试高德逆地址编码
      * @throws Exception
      */
+    @Test
     public void testRegeo() throws Exception {
         String url="https://restapi.amap.com/v3/geocode/regeo";
         Response response= FastHttpClient.get().url(url)
                 .addParams("key",amap_key)
-                .addParams("location","116.481488,39.990464")
+                .addParams("location","121.551919,31.281297")
+                .build().execute();
+        String result=response.body().string();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testAround() throws Exception {
+        String url="https://restapi.amap.com/v3/place/around";
+        Response response= FastHttpClient.get().url(url)
+                .addParams("key",amap_key)
+                .addParams("location","121.551919,31.281297")
+                //.addParams("keywords","紫藤苑")
+                .addParams("offset","20")
+                .addParams("page","2")
                 .build().execute();
         String result=response.body().string();
         System.out.println(result);
