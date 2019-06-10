8d3af36d42ebb9a16a20e5e4c2358bb852b6fac2
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-06-10 17:07:56 +0800

    高德地图附近api

---
+++ collect-boot/src/main/java/com/tzj/collect/api/ali/param/AmapAroundParam.java
@@ -0,0 +1,22 @@
+package com.tzj.collect.api.ali.param;
+
+import com.tzj.module.easyopen.doc.annotation.ApiDocField;
+
+import java.io.Serializable;
+
+public class AmapAroundParam implements Serializable {
+
+    private static final long serialVersionUID = 6981800187882832233L;
+
+    @ApiDocField(description = "经纬度",required=true)
+    private String location;
+
+    @ApiDocField(description = "搜索关键字，可以为空",required = false)
+    private String searchKey;
+
+    @ApiDocField(description = "每页条数",required = true)
+    private String offset;
+
+    @ApiDocField(description = "第几页",required = true)
+    private String page;
+}
--- collect-boot/src/test/java/api/ali/TokenFlushApiTest.java
+++ collect-boot/src/test/java/api/ali/TokenFlushApiTest.java
@@ -4,20 +4,24 @@ import com.alibaba.fastjson.JSON;
 import io.itit.itf.okhttp.FastHttpClient;
 import io.itit.itf.okhttp.Response;
 
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.UUID;
 
 /**
  * @Author 胡方明（12795880@qq.com）
  **/
 public class TokenFlushApiTest {
     public static void main(String[] args) throws Exception {
-        String api="http://localhost:8080/ali/api";
+        String api="http://localhost:9090/ali/api";
 
 
         HashMap<String,Object> param=new HashMap<>();
         param.put("name","token.flush");
         param.put("version","1.0");
         param.put("format","json");
+        param.put("nonce", UUID.randomUUID().toString());
+        param.put("timestamp", Calendar.getInstance().getTimeInMillis());
         param.put("token","3F3TEMH74565Q5QORHNPE76UZM6VT4JPWVV4OPUNTGAXLLRLC6B5GYU3LW34YHVNOEFL2LXPVT24UAJWCBI7NJ42KSYJ2KXG2OVQSA6ZMU4VMMCLQUKIRXAWTX2BD3K6MDOZDBJ4Q62CYGOB7DVAUP4CYQAHL3JSQRIG7P2UO77IZBN7W3E4RZK42VEEUWCHGAZLS7LGRB4EVIIYSQVYYSGAETEUZC4JUVVV2UDRKIOBGXURUGYCOGKTBVFLZYU2QFPF2G4I7DVNKBWCOFWBQDLZLJYEDSPIL6T46KLPZ4O2ZIFJROTQ");
 
         	
