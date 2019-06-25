f25d4e36ebed28f50a4cd5620baafaa81446d34e
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-06-25 16:53:08 +0800

    用户表分表

---
+++ collect-boot/src/test/java/shard/ShardMemberTest.java
@@ -0,0 +1,13 @@
+package shard;
+
+import io.itit.itf.okhttp.FastHttpClient;
+import io.itit.itf.okhttp.Response;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class ShardMemberTest extends TestCase {
+    @Test
+    public void testRegeo() throws Exception {
+        System.out.println(2088112032501924L%40);
+    }
+}
