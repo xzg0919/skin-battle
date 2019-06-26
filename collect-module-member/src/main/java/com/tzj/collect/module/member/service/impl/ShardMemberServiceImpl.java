f2bd34867403c97b153eaf6313d1af59e7e95ffc
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-06-26 15:41:38 +0800

    用户分表

---
+++ collect-module-member/src/main/java/com/tzj/collect/module/member/service/impl/ShardMemberServiceImpl.java
@@ -0,0 +1,15 @@
+package com.tzj.collect.module.member.service.impl;
+
+import com.tzj.collect.module.member.service.ShardMemberService;
+
+public class ShardMemberServiceImpl implements ShardMemberService {
+    @Override
+    public String getTableNameByAliId(String aliId) {
+        return null;
+    }
+
+    @Override
+    public boolean updateMemberByAliId() {
+        return false;
+    }
+}
