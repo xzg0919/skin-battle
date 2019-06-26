09713b0a2564b8ebf8d4e4083f3a26f4f2656bd2
sgmark <sgmark@aliyun.com>
2019-06-26 16:32:32 +0800

    合并

--- collect-boot/src/main/java/com/tzj/collect/service/impl/FlcxTypeServiceImpl.java
+++ collect-boot/src/main/java/com/tzj/collect/service/impl/FlcxTypeServiceImpl.java
@@ -1,19 +1,21 @@
 package com.tzj.collect.service.impl;
 
-import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.tzj.collect.entity.FlcxLexicon;
+import com.tzj.collect.entity.FlcxLexiconType;
 import com.tzj.collect.entity.FlcxType;
 import com.tzj.collect.mapper.FlcxLexiconMapper;
+import com.tzj.collect.mapper.FlcxLexiconTypeMapper;
 import com.tzj.collect.mapper.FlcxTypeMapper;
-import com.tzj.collect.service.FlcxLexiconService;
 import com.tzj.collect.service.FlcxTypeService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,12 +31,49 @@ public class FlcxTypeServiceImpl extends ServiceImpl<FlcxTypeMapper, FlcxType> i
     @Resource
     private FlcxTypeMapper flcxTypeMapper;
 
+    @Resource
+    private FlcxLexiconMapper flcxLexiconMapper;
+
+    @Resource
+    private FlcxLexiconTypeMapper flcxLexiconTypeMapper;
+
     @Override
     public Map typeList() {
         HashMap<String, Object> map = new HashMap<>();
         map.put("typeList", flcxTypeMapper.selectList(new EntityWrapper<FlcxType>().eq("del_flag", 0).eq("level_", 0).eq("parent_id", 0)));
         return map;
     }
-
+    @Override
+    @Transactional(readOnly = false)
+    public void inputLinAndType(List<Map<String, String>> mapList) {
+        List<FlcxType> flcxTypes = flcxTypeMapper.selectList(new EntityWrapper<FlcxType>().eq("del_flag", 0).eq("level_", 1));
+        mapList.parallelStream().forEach(map -> {
+            FlcxLexicon flcxLexicon = new FlcxLexicon();
+            flcxLexicon.setName(map.get("name"));
+            if ("是".equals(map.get("recover"))){
+                flcxLexicon.setRecover("1");
+            }else {
+                flcxLexicon.setRecover("0");
+            }
+            try {
+                flcxLexiconMapper.insert(flcxLexicon);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            String[] stringSet = map.get("type").split("/");
+            List<String> stringList = Arrays.asList(stringSet);
+            //保存类型
+            stringList.stream().forEach(string -> {
+                flcxTypes.stream().forEach(flcxType -> {
+                    if (flcxType.getName().equals(string)){
+                        FlcxLexiconType flcxLexiconType = new FlcxLexiconType();
+                        flcxLexiconType.setLexiconId(flcxLexicon.getId());
+                        flcxLexiconType.setTypeId(flcxType.getId());
+                        flcxLexiconTypeMapper.insert(flcxLexiconType);
+                    }
+                });
+            });
+        });
+    }
 
 }
