3dac1c31b19abfb97e8cf58a2215e749db65671a
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-07-01 16:05:45 +0800

    MQ监听器

---
+++ collect-module-task/src/main/java/com/tzj/collect/module/task/mq/SearchKeywordsListenter.java
@@ -0,0 +1,22 @@
+package com.tzj.collect.module.task.mq;
+
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 查询消息监听器
+ */
+@Component
+public class SearchKeywordsListenter implements ChannelAwareMessageListener {
+
+    protected final static Logger logger = LoggerFactory.getLogger(SearchKeywordsListenter.class);
+
+    @Override
+    public void onMessage(Message message, Channel channel) throws Exception {
+        logger.info("收到的消息： {}",message.getBody());
+    }
+}
