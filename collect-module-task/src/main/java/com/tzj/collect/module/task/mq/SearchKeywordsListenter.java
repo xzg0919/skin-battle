ada6a66d28ff62b4956e67cbdf5b903a7f0cf2a1
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-07-01 18:03:26 +0800

    MQ监听器

--- collect-module-task/src/main/java/com/tzj/collect/module/task/mq/SearchKeywordsListenter.java
+++ collect-module-task/src/main/java/com/tzj/collect/module/task/mq/SearchKeywordsListenter.java
@@ -1,11 +1,14 @@
 package com.tzj.collect.module.task.mq;
 
+import com.alibaba.fastjson.JSON;
 import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
 
 /**
  * 查询消息监听器
@@ -17,10 +20,23 @@ public class SearchKeywordsListenter implements ChannelAwareMessageListener {
 
     @Override
     public void onMessage(Message message, Channel channel) throws Exception {
-        logger.info("收到的消息： {}",new String(message.getBody()));
+
+        String jsonMessage = new String(message.getBody(), "UTF-8");
+        HashMap<String, Object> goodsPriceObject = JSON.parseObject(jsonMessage, HashMap.class);
+
+        //把接收到的消息，保存到数据库
+
+
+
+
+
+
+
+
+        //logger.info("收到的消息： {}",new String(message.getBody()));
 
         //消息确认
-        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        //channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
 
 
 
