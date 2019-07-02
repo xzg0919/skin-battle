f223d1956b289f7f117e461fe1d0cabcfa2f2086
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-07-02 10:04:55 +0800

    垃圾分类查询模块独立

--- collect-module-task/src/main/java/com/tzj/collect/module/task/config/RabbitMqConfig.java
+++ collect-module-task/src/main/java/com/tzj/collect/module/task/config/RabbitMqConfig.java
@@ -1,25 +1,18 @@
 package com.tzj.collect.module.task.config;
 
 
+import com.tzj.collect.config.ExchangeConfig;
+import com.tzj.collect.config.QueueConfig;
 import com.tzj.collect.module.task.mq.SearchKeywordsListenter;
 import org.springframework.amqp.core.AcknowledgeMode;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
-
-    public static final String EXCHANGE = "laji_search";
-
-    public static final String SEARCH_KEYWORDS_QUEUE_ROUTE = "search_keywords_queue_route";
 
     @Autowired
     private QueueConfig queueConfig;
@@ -35,31 +28,6 @@ public class RabbitMqConfig {
      */
     @Autowired
     private ConnectionFactory connectionFactory;
-
-    /**
-     * 将消息队列1和交换机进行绑定
-     */
-    @Bean
-    public Binding bindingSearchKeywordQueue() {
-        return BindingBuilder.bind(queueConfig.searchKeywordsQueue()).to(exchangeConfig.directExchange()).with(RabbitMqConfig.SEARCH_KEYWORDS_QUEUE_ROUTE);
-    }
-
-    /**
-     * 定义rabbit template用于数据的接收和发送
-     *
-     * @return
-     */
-    @Bean
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(jsonMessageConverter());
-        return template;
-    }
-
-    @Bean
-    public MessageConverter jsonMessageConverter(){
-        return new Jackson2JsonMessageConverter();
-    }
 
     @Bean
     public SimpleMessageListenerContainer searchKeywordsListenterContainer(){
