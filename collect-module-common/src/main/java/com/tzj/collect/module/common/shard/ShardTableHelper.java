8f810178507d1037bc2c87334cf204763f39f5d2
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-07-02 13:07:21 +0800

    分表插入

--- collect-module-common/src/main/java/com/tzj/collect/module/common/shard/ShardTableHelper.java
+++ collect-module-common/src/main/java/com/tzj/collect/module/common/shard/ShardTableHelper.java
@@ -1,13 +1,37 @@
 package com.tzj.collect.module.common.shard;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.util.Date;
+
 /**
  * 分表规则
  */
 public class ShardTableHelper {
+    /**
+     * 根据取模做分表
+     * @param tableName
+     * @param shardSource
+     * @param modeling
+     * @return
+     */
     public static String getTableNameByModeling(String tableName,Long shardSource,int modeling){
         long num=shardSource%modeling;
         StringBuffer sb=new StringBuffer();
-        sb.append(tableName).append("_");
+        sb.append(tableName).append("_").append(num);
+        return sb.toString();
+    }
+
+    /**
+     * 根据年月日做分表
+     * @param tableName
+     * @param date
+     * @return
+     */
+    public static String getTableNameByDay(String tableName, Date date){
+        String dateString= DateFormatUtils.format(new Date(), "yyyyMMdd");
+        StringBuffer sb=new StringBuffer();
+        sb.append(tableName).append("_").append(dateString);
         return sb.toString();
     }
 }
