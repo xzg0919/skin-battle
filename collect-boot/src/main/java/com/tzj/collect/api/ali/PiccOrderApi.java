c532f3205f4cc151cb2649322b5dbf0bb063f778
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2019-11-13 14:30:16 +0800

    结构调整

--- collect-boot/src/main/java/com/tzj/collect/api/ali/PiccOrderApi.java
+++ collect-boot/src/main/java/com/tzj/collect/api/ali/PiccOrderApi.java
@@ -1,22 +1,18 @@
 package com.tzj.collect.api.ali;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.taobao.api.ApiException;
 import com.tzj.collect.common.util.MemberUtils;
 import com.tzj.collect.core.param.ali.PiccOrderBean;
 import com.tzj.collect.core.service.PiccOrderService;
 import com.tzj.collect.entity.Member;
 import com.tzj.collect.entity.PiccOrder;
+import static com.tzj.common.constant.TokenConst.ALI_API_COMMON_AUTHORITY;
 import com.tzj.module.api.annotation.Api;
 import com.tzj.module.api.annotation.ApiService;
 import com.tzj.module.api.annotation.RequiresPermissions;
-import com.tzj.module.api.annotation.SignIgnore;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-
-import static com.tzj.common.constant.TokenConst.ALI_API_COMMON_AUTHORITY;
 
 @ApiService
 public class PiccOrderApi {
@@ -24,44 +20,45 @@ public class PiccOrderApi {
     @Autowired
     private PiccOrderService piccOrderService;
 
-
     /**
      * 添加picc用户下的保险单
+     *
      * @author 王灿
      * @param
      * @return List<Order>:未完成的订单列表
      */
     @Api(name = "piccOrder.insertPiccOrder", version = "1.0")
     @RequiresPermissions(values = ALI_API_COMMON_AUTHORITY)
-    public String insertPiccOrder(PiccOrderBean piccOrderBean) throws ApiException {
+    public String insertPiccOrder(PiccOrderBean piccOrderBean) throws Exception {
         Member member = MemberUtils.getMember();
         //查询该用户是否有保单
         List<PiccOrder> piccOrderList = piccOrderService.selectList(new EntityWrapper<PiccOrder>().eq("ali_user_id", member.getAliUserId()).eq("del_flag", 0).eq("insurance_id", piccOrderBean.getInsuranceId()));
         if (piccOrderList != null) {
-            for (PiccOrder piccOrder:piccOrderList) {
-                if (piccOrder.getStatus().getValue() != PiccOrder.PiccOrderType.NOOPEN.getValue()){
+            for (PiccOrder piccOrder : piccOrderList) {
+                if (piccOrder.getStatus().getValue() != PiccOrder.PiccOrderType.NOOPEN.getValue()) {
                     return "您已存在保险或保险正在审核";
                 }
             }
         }
-        return piccOrderService.insertPiccOrder(member.getAliUserId(),piccOrderBean);
+        return piccOrderService.insertPiccOrder(member.getAliUserId(), piccOrderBean);
     }
 
     /**
      * 用户收取绿色能量
+     *
      * @author 王灿
      * @param
      * @return List<Order>:未完成的订单列表
      */
     @Api(name = "piccOrder.updatePiccWater", version = "1.0")
     @RequiresPermissions(values = ALI_API_COMMON_AUTHORITY)
-    public String updatePiccWater(PiccOrderBean piccOrderBean) throws ApiException {
+    public String updatePiccWater(PiccOrderBean piccOrderBean) throws Exception {
         Member member = MemberUtils.getMember();
         String piccWaterId = piccOrderBean.getId();
-        if(StringUtils.isBlank(piccWaterId)){
+        if (StringUtils.isBlank(piccWaterId)) {
             piccWaterId = "0";
         }
-        return piccOrderService.updatePiccWater(member.getAliUserId(),Integer.parseInt(piccOrderBean.getId()));
+        return piccOrderService.updatePiccWater(member.getAliUserId(), Integer.parseInt(piccOrderBean.getId()));
     }
 
 }
--- collect-boot/src/main/java/com/tzj/collect/api/enterprise/EnterpriseProductApi.java
+++ collect-boot/src/main/java/com/tzj/collect/api/enterprise/EnterpriseProductApi.java
@@ -1,9 +1,7 @@
 package com.tzj.collect.api.enterprise;
 
-
 import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.taobao.api.ApiException;
 import com.tzj.collect.common.util.EnterpriseUtils;
 import com.tzj.collect.core.param.enterprise.EnterpriseProductBean;
 import com.tzj.collect.core.service.CategoryService;
@@ -11,23 +9,22 @@ import com.tzj.collect.core.service.EnterpriseProductService;
 import com.tzj.collect.entity.Category;
 import com.tzj.collect.entity.EnterpriseAccount;
 import com.tzj.collect.entity.EnterpriseProduct;
+import static com.tzj.common.constant.TokenConst.ENTERPRISE_API_COMMON_AUTHORITY;
 import com.tzj.module.api.annotation.Api;
 import com.tzj.module.api.annotation.ApiService;
 import com.tzj.module.api.annotation.RequiresPermissions;
 import com.tzj.module.api.annotation.SignIgnore;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static com.tzj.common.constant.TokenConst.ENTERPRISE_API_COMMON_AUTHORITY;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * 依旧换新产品信息
  */
 @ApiService
-public class EnterpriseProductApi  extends  Exception{
+public class EnterpriseProductApi extends Exception {
+
     @Autowired
     private EnterpriseProductService enterpriseProductService;
     @Autowired
@@ -37,20 +34,21 @@ public class EnterpriseProductApi  extends  Exception{
      * 更改/新增以旧换新的产品信息
      *
      * 王灿
+     *
      * @param enterpriseProductBean
      * @return
      */
     @Api(name = "enterprise.updateEnterpriseProduct", version = "1.0")
     @SignIgnore
     @RequiresPermissions(values = ENTERPRISE_API_COMMON_AUTHORITY)
-    public Object updateEnterpriseProduct(EnterpriseProductBean enterpriseProductBean) throws ApiException {
+    public Object updateEnterpriseProduct(EnterpriseProductBean enterpriseProductBean) throws Exception {
         EnterpriseAccount enterpriseAccount = EnterpriseUtils.getEnterpriseAccount();
-        return  enterpriseProductService.updateEnterpriseProduct(enterpriseProductBean,enterpriseAccount.getEnterpriseId());
+        return enterpriseProductService.updateEnterpriseProduct(enterpriseProductBean, enterpriseAccount.getEnterpriseId());
     }
 
     /**
-     * 以旧换新的产品列表
-     * 王灿
+     * 以旧换新的产品列表 王灿
+     *
      * @param enterpriseProductBean
      * @return
      */
@@ -58,25 +56,25 @@ public class EnterpriseProductApi  extends  Exception{
     @SignIgnore
     @RequiresPermissions(values = ENTERPRISE_API_COMMON_AUTHORITY)
     @DS("slave")
-    public Object EnterpriseProductList(EnterpriseProductBean enterpriseProductBean) throws ApiException {
+    public Object EnterpriseProductList(EnterpriseProductBean enterpriseProductBean) throws Exception {
         EnterpriseAccount enterpriseAccount = EnterpriseUtils.getEnterpriseAccount();
         EntityWrapper<EnterpriseProduct> wrapper = new EntityWrapper<>();
         wrapper.eq("enterprise_id", enterpriseAccount.getEnterpriseId());
         wrapper.eq("del_flag", 0);
         int count = enterpriseProductService.selectCount(wrapper);
-        wrapper.orderBy("create_date",false);
-        wrapper.last(" LIMIT "+((enterpriseProductBean.getPageBean().getPageNumber()-1)*enterpriseProductBean.getPageBean().getPageSize())+","+enterpriseProductBean.getPageBean().getPageSize());
+        wrapper.orderBy("create_date", false);
+        wrapper.last(" LIMIT " + ((enterpriseProductBean.getPageBean().getPageNumber() - 1) * enterpriseProductBean.getPageBean().getPageSize()) + "," + enterpriseProductBean.getPageBean().getPageSize());
         List<EnterpriseProduct> enterpriseProducts = enterpriseProductService.selectList(wrapper);
-        Map<String,Object> map = new HashMap<String,Object>();
-        map.put("enterpriseProducts",enterpriseProducts);
-        map.put("count",count);
-        map.put("pageNumber",enterpriseProductBean.getPageBean().getPageNumber());
-        return  map;
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("enterpriseProducts", enterpriseProducts);
+        map.put("count", count);
+        map.put("pageNumber", enterpriseProductBean.getPageBean().getPageNumber());
+        return map;
     }
 
     /**
-     * 以旧换新根据产品Id查询产品的详细信息
-     * 王灿
+     * 以旧换新根据产品Id查询产品的详细信息 王灿
+     *
      * @param enterpriseProductBean
      * @return
      */
@@ -84,11 +82,12 @@ public class EnterpriseProductApi  extends  Exception{
     @SignIgnore
     @RequiresPermissions(values = ENTERPRISE_API_COMMON_AUTHORITY)
     public Object EnterpriseProductById(EnterpriseProductBean enterpriseProductBean) {
-        return  enterpriseProductService.selectById(enterpriseProductBean.getId());
+        return enterpriseProductService.selectById(enterpriseProductBean.getId());
     }
+
     /**
-     * 获取分类列表
-     * 王灿
+     * 获取分类列表 王灿
+     *
      * @param
      * @return
      */
@@ -96,12 +95,12 @@ public class EnterpriseProductApi  extends  Exception{
     @SignIgnore
     @RequiresPermissions(values = ENTERPRISE_API_COMMON_AUTHORITY)
     public Object categoryList() {
-        return categoryService.selectList(new EntityWrapper<Category>().eq("level_",0).eq("title",1).eq("del_flag",0));
+        return categoryService.selectList(new EntityWrapper<Category>().eq("level_", 0).eq("title", 1).eq("del_flag", 0));
     }
 
     /**
-     * 删除商品
-     * 王灿
+     * 删除商品 王灿
+     *
      * @param
      * @return
      */
@@ -114,6 +113,5 @@ public class EnterpriseProductApi  extends  Exception{
         enterpriseProductService.updateById(enterpriseProduct);
         return "操作成功";
     }
-
 
 }
