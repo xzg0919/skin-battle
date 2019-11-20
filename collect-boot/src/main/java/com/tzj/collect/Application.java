041625e0e855dbc3460a914618d765193c24f07c
wang <1021618676@qq.com>
2019-11-20 09:17:09 +0800

    collect

--- collect-boot/src/main/java/com/tzj/collect/Application.java
+++ collect-boot/src/main/java/com/tzj/collect/Application.java
@@ -1,12 +1,16 @@
 package com.tzj.collect;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.apache.catalina.connector.Connector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -29,5 +33,19 @@ public class Application {
         app.run(args);
         logger.info("PortalApplication is success!");
     }
- 
+
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+                connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+            }
+        });
+        return factory;
+    }
 }
