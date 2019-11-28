9242ba521acb26639264c6b93887a66cc85288a6
sgmark <sgmark@aliyun.com>
2019-11-28 17:51:06 +0800

    刷脸验证

---
+++ collect-module-api/src/main/java/com/tzj/collect/core/param/iot/SmilePayBean.java
@@ -0,0 +1,28 @@
+package com.tzj.collect.core.param.iot;
+
+import lombok.Data;
+
+/**
+ * 刷脸bean
+ * @author sgmark
+ * @create 2019-11-26 10:31
+ **/
+@Data
+public class SmilePayBean {
+
+    private String apdidToken;
+
+    private String appName;
+
+    private String appVersion;
+    /**
+     * 生物识别元信息
+     */
+    private String bioMetaInfo;
+
+    private String fToken;
+    /**
+     * 所要登录的设备编号的硬件编号
+     */
+    private String hardwareCode;
+}
