e722f7c417b0c23cf1dc49aeb81e2c3d00e06c92
diagcn.shark@gmail.com <diagcn.shark@gmail.com>
2020-01-06 14:53:40 +0800

    清楚api 文档注解，后面api框架升级将取消这块的功能

--- collect-module-api/src/main/java/com/tzj/collect/core/param/app/OrderPayParam.java
+++ collect-module-api/src/main/java/com/tzj/collect/core/param/app/OrderPayParam.java
@@ -1,6 +1,6 @@
 package com.tzj.collect.core.param.app;
 
-import com.tzj.module.easyopen.doc.annotation.ApiDocField;
+
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,10 +8,10 @@ import java.math.BigDecimal;
 @Data
 public class OrderPayParam {
 
-    @ApiDocField(description = "收呗订单ID")
+
     private int orderId;
 
-    @ApiDocField(description = "支付金额")
+
     private BigDecimal price;
 
     private String voucherId;//优惠券Id
