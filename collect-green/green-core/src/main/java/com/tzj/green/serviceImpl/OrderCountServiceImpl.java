f2dfc2220b8b14715a2cfe940538963fe1af70ae
山 <you@example.com>
2020-05-08 13:11:21 +0800

    数据放入缓存

--- collect-green/green-core/src/main/java/com/tzj/green/serviceImpl/OrderCountServiceImpl.java
+++ collect-green/green-core/src/main/java/com/tzj/green/serviceImpl/OrderCountServiceImpl.java
@@ -1,9 +1,11 @@
 package com.tzj.green.serviceImpl;
 
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.tzj.green.common.redis.RedisUtil;
 import com.tzj.green.mapper.OrderCountMapper;
 import com.tzj.green.service.OrderCountService;
 import org.apache.poi.ss.formula.functions.T;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,8 @@ public class OrderCountServiceImpl extends ServiceImpl<OrderCountMapper, T> impl
 
     @Resource
     private OrderCountMapper orderCountMapper;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Override
     public List<Map<String, Object>> getOrderCount() {
@@ -26,30 +30,32 @@ public class OrderCountServiceImpl extends ServiceImpl<OrderCountMapper, T> impl
 
     @Override
     public Object getOrderCount1() {
-
-        List<Map<String, Object>>list=orderCountMapper.getOrderCountList1();
-        List<Map<String, Object>>listCount = new ArrayList<>();
-        Map <String, Object> map= null;
-        for(int i=0,j = list.size(); i<j;i++){
-            map = list.get(i);
-            Map <String, Object> mapCount = new HashMap<>();
-            mapCount.put("rfid",map.get("userNo"));
-            mapCount.put("garbageType",map.get("garbageTypeNumber"));
-            mapCount.put("recycleType",map.get("recycleTypeNumber"));
-            mapCount.put("weight",map.get("amount"));
-            mapCount.put("classifyState","1");
-            mapCount.put("weighTime",map.get("createTime"));
-            mapCount.put("dealNum","JK"+map.get("id")+map.get("recyclerId"));
-            mapCount.put("company","解衣科技");
-            mapCount.put("equipNo","");
-            listCount.add(mapCount);
+        List<Object> listOrder = redisUtil.lGet("order", 0, -1);
+        if (listOrder == null||listOrder.isEmpty()) {
+            List<Map<String, Object>> list = orderCountMapper.getOrderCountList1();
+            Map<String, Object> map = null;
+            for (int i = 0, j = list.size(); i < j; i++) {
+                map = list.get(i);
+                Map<String, Object> mapCount = new HashMap<>();
+                mapCount.put("rfid", map.get("userNo"));
+                mapCount.put("garbageType", map.get("garbageTypeNumber"));
+                mapCount.put("recycleType", map.get("recycleTypeNumber"));
+                mapCount.put("weight", map.get("amount"));
+                mapCount.put("classifyState", "1");
+                mapCount.put("weighTime", map.get("createTime"));
+                mapCount.put("dealNum", "JK" + map.get("id") + map.get("recyclerId"));
+                mapCount.put("company", "解衣科技");
+                mapCount.put("equipNo", "");
+                listOrder.add(mapCount);
+            }
+            redisUtil.lSet("order",listOrder,60*60*24);
         }
-        Integer count=list.size();
+        /*Integer count=list.size();
         Map<String, Object> resultMap = new HashMap<>();
         Map<String, Object> pagination = new HashMap<>();
         pagination.put("total",count);
         resultMap.put("pagination", pagination);
-        resultMap.put("orderList", listCount);
-        return resultMap;
+        resultMap.put("orderList", listCount);*/
+         return listOrder;
     }
 }
