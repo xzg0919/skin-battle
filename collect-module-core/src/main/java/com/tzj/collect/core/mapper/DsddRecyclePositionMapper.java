c2c3e67f6ddabe73f742ce0416a91b671c898a3d
Yang <3142067375@qq.com>
2020-05-26 13:12:58 +0800

    定时定点订单详情

---
+++ collect-module-core/src/main/java/com/tzj/collect/core/mapper/DsddRecyclePositionMapper.java
@@ -0,0 +1,9 @@
+package com.tzj.collect.core.mapper;
+
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.tzj.collect.entity.DsddRecyclePosition;
+
+public interface DsddRecyclePositionMapper extends BaseMapper<DsddRecyclePosition> {
+
+}
