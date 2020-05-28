cd9315a0cfc0d135b03c3cb70528b3b29a24355f
wang <1021618676@qq.com>
2020-05-28 15:25:20 +0800

    collect

--- collect-module-api/src/main/java/com/tzj/collect/core/service/GongYiCharityService.java
+++ collect-module-api/src/main/java/com/tzj/collect/core/service/GongYiCharityService.java
@@ -1,5 +1,6 @@
 package com.tzj.collect.core.service;
 
+import com.alipay.api.response.AlipaySystemOauthTokenResponse;
 import com.taobao.api.response.AlibabaCharityUseractionSyncResponse;
 
 public interface GongYiCharityService {
@@ -7,5 +8,7 @@ public interface GongYiCharityService {
     AlibabaCharityUseractionSyncResponse.UserActionSyncResult gongYiCharityUser(String aliUserId);
 
     Long gongYiCharityCharityTime(String aliUserId);
+
+    AlipaySystemOauthTokenResponse selectUserToken(String userCode);
 
 }
