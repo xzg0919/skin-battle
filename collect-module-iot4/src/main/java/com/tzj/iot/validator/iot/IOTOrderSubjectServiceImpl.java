915ebe9363c04c594ce5b33dea6b1bd4cd59fbe0
630333719 <630333719@qq.com>
2021-04-19 13:47:09 +0800

    iot

---
+++ collect-module-iot4/src/main/java/com/tzj/iot/validator/iot/IOTOrderSubjectServiceImpl.java
@@ -0,0 +1,73 @@
+package com.tzj.iot.validator.iot;
+
+import com.tzj.collect.core.service.CompanyEquipmentService;
+import com.tzj.collect.core.service.MemberService;
+import com.tzj.collect.entity.CompanyEquipment;
+import com.tzj.collect.entity.Member;
+import com.tzj.module.api.entity.Subject;
+import com.tzj.module.api.service.SubjectService;
+import com.tzj.module.easyopen.util.EhCache2Utils;
+import net.sf.ehcache.CacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+import static com.tzj.collect.common.constant.TokenConst.ALI_API_COMMON_AUTHORITY;
+import static com.tzj.collect.common.constant.TokenConst.EQUIPMENT_APP_API_COMMON_AUTHORITY;
+
+
+@Service("iotOrderApiSubjectServiceImpl")
+public class IOTOrderSubjectServiceImpl implements SubjectService {
+
+    private final String cacheName = "iotOrderAppCache";
+
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Override
+    public Subject getSubjectByTokenSubject(String token,String key, Map<String, Object> map) {
+        Subject subjectCache=getSubjectCache(token);
+        if(subjectCache!=null){
+            return subjectCache;
+        }
+        Member member = memberService.selectMemberByAliUserId(key);
+
+        Subject subject=new Subject();
+        subject.setId(member.getId().toString());
+        subject.setName(member.getName());
+        subject.setUser(member);
+
+        LinkedList<String> authorities=new LinkedList<>();
+        authorities.add(ALI_API_COMMON_AUTHORITY);
+        subject.setAuthorities(authorities);
+
+        //TODO： 分表后，后面需要取消 User 对象
+
+
+        setSubjectCache(token,subject);
+
+        return subject;
+    }
+
+    @Override
+    public void setSubjectCache(String token,Subject subject) {
+        EhCache2Utils.setCache(token,subject,cacheManager,cacheName);
+    }
+
+    @Override
+    public Subject getSubjectCache(String token) {
+        return EhCache2Utils.getSubjectByCache(cacheManager,cacheName,token);
+    }
+
+
+    @Override
+    public void removeSubjectCache(String token) {
+        EhCache2Utils.removeCache(token,cacheManager,cacheName);
+    }
+}
