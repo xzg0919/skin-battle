23aa28ad16da1e88b58453bdd3208c878d44c7ed
xiangzhongguo <xiangzg0919@163.com>
2022-03-15 17:45:46 +0800

    提交

---
+++ collect-module-api/src/main/java/com/tzj/collect/common/util/HttpUtils.java
@@ -0,0 +1,30 @@
+package com.tzj.collect.common.util;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class HttpUtils {
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static OkHttpClient client = new OkHttpClient();
+
+    public static Response post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response;
+    }
+
+    public static Response get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response;
+    }
+
+}
