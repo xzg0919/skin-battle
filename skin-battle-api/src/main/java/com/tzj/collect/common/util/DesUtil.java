5b93b3fc448aaeb6f74a09c696c8e2a79340365b
xiangzhongguo <xiangzg0919@163.com>
2022-08-08 12:16:54 +0800

    初始化

---
+++ skin-battle-api/src/main/java/com/tzj/collect/common/util/DesUtil.java
@@ -0,0 +1,160 @@
+package com.tzj.collect.common.util;
+
+
+import cmb.netpayment.Security;
+import com.sun.xml.internal.fastinfoset.Encoder;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+
+/**
+ * DES加密 解密算法
+ *
+ * @author
+ * @date
+ */
+public class DesUtil {
+    //加密算法
+    private final static String DES = "DES";
+    /**
+     * 加密/解密算法-工作模式-填充模式
+     */
+    private static final String CIPHER_ALGORITHM = "DES/ECB/PKCS5Padding";
+    //编码
+    private final static String ENCODE = "UTF-8";
+    //秘钥
+    private final static String defaultKey = "cmbtest1"; //默认秘钥
+
+    /**
+     * 使用 默认key 加密
+     * @return String
+     * @author
+     * @date
+     */
+    public static String encrypt(String data) throws Exception {
+        byte[] bt = encrypt(data.getBytes(ENCODE), defaultKey.getBytes(ENCODE));
+        String strs = new BASE64Encoder().encode(bt);
+        return strs;
+    }
+
+    /**
+     * 使用 默认key 解密
+     * @return String
+     * @author
+     * @date
+     */
+    public static String decrypt(String data) throws IOException, Exception {
+        if (data == null)
+            return null;
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] buf = decoder.decodeBuffer(data);
+        byte[] bt = decrypt(buf, defaultKey.getBytes(ENCODE));
+        return new String(bt, ENCODE);
+    }
+
+
+    /**
+     * Description 根据键值进行加密
+     *
+     * @param data
+     * @param key
+     *            加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    public static String encrypt(String data, String key) throws Exception {
+        byte[] bt = encrypt(data.getBytes(ENCODE), defaultKey.getBytes(ENCODE));
+        String strs = new BASE64Encoder().encode(bt);
+        return strs;
+    }
+
+    /**
+     * Description 根据键值进行解密
+     *
+     * @param data
+     * @param key
+     *            加密键byte数组
+     * @return
+     * @throws IOException
+     * @throws Exception
+     */
+    public static String decrypt(String data, String key) throws IOException,
+            Exception {
+        if (data == null)
+            return null;
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] buf = decoder.decodeBuffer(data);
+        byte[] bt = decrypt(buf, key.getBytes(ENCODE));
+        return new String(bt, ENCODE);
+    }
+
+    /**
+     * Description 根据键值进行加密
+     *
+     * @param data
+     * @param key
+     *            加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);  //加密算法
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成加密操作
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM); //这一步定义加密方式，填充方式等
+
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * Description 根据键值进行解密
+     *
+     * @param data
+     * @param key
+     *            加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES); //加密算法
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成解密操作
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);   //这一步定义加密方式，填充方式等
+
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+
+    public static boolean VerifySignature(String signData,String publicKeyPath) throws Exception {
+        Security verifier = new Security(publicKeyPath);
+        return verifier.checkInfoFromBank(signData.getBytes(Encoder. UTF_8));
+    }
+}
